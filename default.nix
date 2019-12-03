9555c59f76f00320b9dabcb95caa05b09ff3ae4f
jD91mZM2 <me@krake.one>
2019-12-03 18:45:35 +0100

    Update pinned nixpkgs

--- default.nix
+++ default.nix
@@ -1,3 +1,8 @@
 { pkgs ? import ./pinned.nix {} }:
 
-(pkgs.callPackage ./Cargo.nix {}).rootCrate.build
+(pkgs.callPackage ./Cargo.nix {}).rootCrate.build.overrideAttrs (_attrs: {
+  postInstall = ''
+    # Remove pointless file which can cause collisions
+    rm $out/lib/link
+  '';
+})
--- pinned.nix
+++ pinned.nix
@@ -2,10 +2,10 @@
 # useful to ensure cachix binary cache gets used.
 
 import (builtins.fetchGit {
-  name = "nixos-19.09-2019-10-27";
+  name = "nixos-19.09-2019-12-03";
   url = https://github.com/nixos/nixpkgs/;
-  # Commit hash for nixos-unstable as of 2019-10-27
+  # Commit hash for nixos-unstable as of 2019-12-03
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-19.09`
   ref = "refs/heads/nixos-19.09";
-  rev = "27a5ddcf747fb2bb81ea9c63f63f2eb3eec7a2ec";
+  rev = "72a2ced252348defc877bea0a8b551272c0be3f9";
 })
